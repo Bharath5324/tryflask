8d7e4746806a2282a7e62c747e78b0b46d783548
Bharath5324 <bharath.s@ieee.org>
2020-08-20 01:00:57 +0530

    User login form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-
+from app.forms import LoginForm
 @app.route("/")
 
 @app.route("/index")
@@ -14,7 +14,15 @@ def index():
         {
             'author': {'username':'Monu'},
             'body': 'I like to tweet so I tweeted'
+        },
+        {
+            'author': {'username':'Vava'},
+            'body': 'My little berdie is all about scary stuff'
         }
-
     ]
     return render_template('index.html', title='Home', user = user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('forms.html', form = form, title="Sign In")
