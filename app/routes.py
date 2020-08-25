7acecd37adde2d04d25693989eb031596eec4612
Bharath5324 <bharath.s@ieee.org>
2020-08-26 00:29:39 +0530

    Till migration repo creation

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 @app.route("/")
-
 @app.route("/index")
 def index():
     user = {'username': 'Bharath'}
@@ -22,7 +21,10 @@ def index():
     ]
     return render_template('index.html', title='Home', user = user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login required for user {}, remember me = {}".format(form.username.data, form.rememberme.data))
+        return redirect(url_for('index'))
     return render_template('forms.html', form = form, title="Sign In")
