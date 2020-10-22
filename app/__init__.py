90fc3ed279894afb53455d5edefe71d0a9caaa49
Bharath5324 <bharath.s@ieee.org>
2020-10-22 23:20:12 +0530

    last commit from hp

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,15 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_login import LoginManager
+import flask_monitoringdashboard as dashboard
+# from flask_restful import Api
 
 app = Flask(__name__)
+login = LoginManager(app)
+dashboard.config.init_from(file='../config.cfg')
+dashboard.bind(app)
+# api = Api(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from .models import User
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
     user = {'username': 'Bharath'}
     posts  = [
+
         {
             'author': {"username": "John"},
             'body' : 'I wanted to tweet so I tweeted'
@@ -23,8 +27,32 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Login required for user {}, remember me = {}".format(form.username.data, form.rememberme.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('index'))
+        login_user(user, remember = form.rememberme.data)
         return redirect(url_for('index'))
-    return render_template('forms.html', form = form, title="Sign In")
+
+    return render_template('login.html', title='Sign In', form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author':user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post#2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
